4e6a3533237f6c778f18e5538a23b8cd3e6d2a04
rakshithn050 <rakshith14022001@gmail.com>
2024-07-12 17:12:31 +0530

    Server Setup

---
+++ react_chat_app_server/index.js
@@ -0,0 +1,35 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import mongoose from "mongoose";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 4000;
+const DB_URL = process.env.DB_URL;
+
+app.use(
+  cors({
+    origin: process.env.CLIENT_BASE_URL,
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
+    credentials: true,
+  })
+);
+
+app.use(cookieParser());
+app.use(express.json());
+
+const server = app.listen(PORT, () => {
+  console.log(`app running on port ${PORT}`);
+});
+
+mongoose
+  .connect(DB_URL)
+  .then(() => {
+    console.log("mongodb connection successful");
+  })
+  .catch((err) => {
+    console.error(err);
+  });
