7ca3572da6cad04113122b158bdc24af253b5cab
rakshithn050 <rakshith14022001@gmail.com>
2024-07-13 11:24:20 +0530

    User Model and ErrorHandler Created

--- react_chat_app_server/index.js
+++ react_chat_app_server/index.js
@@ -33,3 +33,9 @@ mongoose
   .catch((err) => {
     console.error(err);
   });
+
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal Server Error";
+  res.status(statusCode).json({ success: false, statusCode, message });
+});
---
+++ react_chat_app_server/models/UserModel.js
@@ -0,0 +1,51 @@
+import mongoose from "mongoose";
+import bcrypt from "bcrypt";
+
+const UserSchema = new mongoose.Schema(
+  {
+    email: {
+      type: String,
+      required: [true, "Email is required"],
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: [true, "Password is required"],
+    },
+    firstName: {
+      type: String,
+      required: false,
+    },
+    lastName: {
+      type: String,
+      required: false,
+    },
+    image: {
+      type: String,
+      required: false,
+      default:
+        "https://img.freepik.com/premium-vector/man-avatar-profile-picture-vector-illustration_268834-538.jpg",
+    },
+    color: {
+      type: Number,
+      required: false,
+    },
+    profileSetup: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  { timestamps: true }
+);
+
+UserSchema.pre("save", async function (next) {
+  if (this.isModified("password") || this.isNew) {
+    const salt = await bcrypt.genSalt();
+    this.password = await bcrypt.hash(this.password, salt);
+  }
+  next();
+});
+
+const User = mongoose.model("User", UserSchema);
+
+export default User;
