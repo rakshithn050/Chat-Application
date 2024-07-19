3ad9a6646bb6260e9d34a2337b2052e65a18f5d7
rakshithn050 <rakshith14022001@gmail.com>
2024-07-19 20:45:45 +0530

    Minor Changes

--- react_chat_app_client/src/components/app/Title.jsx
+++ react_chat_app_client/src/components/app/Title.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Title = ({ text }) => {
   return (
     <h6 className="uppercase tracking-widest text-neutral-400 pl-10 font-light text-opacity-90 text-sm">
-      {text}
+      {text} title
     </h6>
   );
 };
